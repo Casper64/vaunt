103b636a6e0384103b1ec68f8e3f2c9b925df5b4
Casper Kuethe <casperck64@gmail.com>
2023-04-10 20:21:30 +0200

    fix admin app upload

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: '../vaunt/admin',
+    outDir: '../src/admin',
     assetsDir: 'admin/assets',
     copyPublicDir: true,
     emptyOutDir: true
